299df6d73ecaeb55a266f31fef0aa902fa62026b
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-08-10 21:38:19 +0200

    Static NodeJS frameworks added.

---
+++ wwwroot/style/app.js
@@ -0,0 +1,2 @@
+﻿import { MDCRipple } from '@material/ripple/index';
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
--- wwwroot/webpack.config.js
+++ wwwroot/webpack.config.js
@@ -1,9 +1,8 @@
-﻿module.exports = [{
-    entry: './style/app.scss',
+﻿const autoprefixer = require('autoprefixer');
+module.exports = [{
+    entry: ['./style/app.scss', './style/app.js'],
     output: {
-        // This is necessary for webpack to compile
-        // But we never use style-bundle.js
-        filename: 'style-bundle.js',
+        filename: 'bundle.js',
     },
     module: {
         rules: [
@@ -19,16 +18,32 @@
                     { loader: 'extract-loader' },
                     { loader: 'css-loader' },
                     {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    {
                         loader: 'sass-loader',
                         options: {
+                            sassOptions: {
+                                includePaths: ['./node_modules']
+                            },
                             // Prefer Dart Sass
                             implementation: require('sass'),
 
                             // See https://github.com/webpack-contrib/sass-loader/issues/804
                             webpackImporter: false,
-                        },
+                        }
                     },
                 ]
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env'],
+                },
             }
         ]
     },
