e5ef72ff2b2f51b760905b70976a6b6862188316
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-08-10 21:48:57 +0200

    Webpack build mode added.

--- wwwroot/style/app.js
+++ wwwroot/style/app.js
@@ -1,2 +1,4 @@
-﻿import { MDCRipple } from '@material/ripple/index';
+﻿console.log("NodeJS => App.js is now working.");
+
+import { MDCRipple } from '@material/ripple/index';
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
