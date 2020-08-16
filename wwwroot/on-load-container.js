06e19b7d2a2bab676d94186a9517aa8395fd5d6b
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-08-16 22:11:39 +0200

    Added all MDC at once and fixed bundle load bug.

---
+++ wwwroot/on-load-container.js
@@ -0,0 +1,8 @@
+﻿function onLoadDo() {
+    var script = document.createElement('script');
+
+    script.setAttribute("src", "./dist/bundle.js");
+    script.setAttribute("async", "");
+
+    document.body.appendChild(script);
+}
