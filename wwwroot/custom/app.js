6593b5587a2bb3e332671b1c5d9ff33aa9430f79
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-08-17 02:50:21 +0200

    Complete implementations for the components correct some animations bugs.

--- wwwroot/custom/app.js
+++ wwwroot/custom/app.js
@@ -1,2 +1,48 @@
 ﻿import * as mdc from 'material-components-web';
+
+// TopAppBar
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new mdc.topAppBar.MDCTopAppBar(topAppBarElement);
+
+// Icon Buttons
+const iconButtonRipple = new mdc.ripple.MDCRipple(document.querySelector('.mdc-icon-button'));
+iconButtonRipple.unbounded = true;
+
+// Textfield
+const textField = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field'));
+
+// List
+const list = new mdc.list.MDCList(document.querySelector('.mdc-list'));
+
+// Checkbox
 const checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('.mdc-checkbox'));
+const formField = new mdc.formField.MDCFormField(document.querySelector('.mdc-form-field'));
+formField.input = checkbox;
+
+// Ripple for List
+const listItemRipples = list.listElements.map((listItemEl) => new mdc.ripple.MDCRipple(listItemEl));
+
+// Tab Bar
+const tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
+// Tab Scroller
+const tabScroller = new mdc.tabScroller.MDCTabScroller(document.querySelector('.mdc-tab-scroller'));
+
+// Tab
+const tabs = [].map.call(document.querySelectorAll('.mdc-tab'), function (el) {
+    return new mdc.tab.MDCTab(el);
+});
+
+// Tab Indicator
+const tabIndicators = [].map.call(document.querySelectorAll('.mdc-tab-indicator'), function (el) {
+    return new mdc.tabIndicator.MDCTabIndicator(el);
+});
+
+// Cards
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function (el) {
+    return new mdc.ripple.MDCRipple(el);
+});
+
+// FAB
+const fabRipple = new mdc.ripple.MDCRipple(document.querySelector('.mdc-fab'));
